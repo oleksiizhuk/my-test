739be81d8fae53ff2825b8906d953c460ce4118e
OleksiiZhuk <oleksiizhuk.att@gmail.com>
2019-07-28 23:51:30 +0300

    add todolist

--- udemy/todo/src/index.js
+++ udemy/todo/src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const el = (
-    <div>
-        <h1>My todo List</h1>
-        <input type="text" placeholder='search'/>
+const TodoList = () => {
+    return (
         <ul>
             <li>Learn React</li>
             <li>Build Awesome app</li>
         </ul>
-    </div>
-);
+    );
+};
 
-ReactDom.render(el, document.getElementById('root'));
+const AppHeader = () => {
+    return <h1>My todo List</h1>;
+};
+
+const SearchPanel = () => {
+    const searchText = 'Type here to search';
+    const searchStyle = {
+        fontSize: '20px'
+    };
+    return <input style={searchStyle} type="text" placeholder={searchText }/>;
+};
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader/>
+            <SearchPanel/>
+            <TodoList/>
+        </div>
+    );
+};
+
+ReactDom.render(<App/>, document.getElementById('root'));

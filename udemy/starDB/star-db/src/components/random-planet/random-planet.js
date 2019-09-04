162fdb13ca5a012cc7ce1a62800fa85c3f5b1ec8
OleksiiZhuk <oleksiizhuk.att@gmail.com>
2019-09-04 14:07:41 +0300

    a

--- udemy/starDB/star-db/src/components/random-planet/random-planet.js
+++ udemy/starDB/star-db/src/components/random-planet/random-planet.js
@@ -16,7 +16,7 @@ export default class randomPlanet extends Component {
             loading: true,
             error: false
         };
-        this.updatePlanet();
+        setInterval(this.updatePlanet, 5000);
     }
 
     onPlanetLoaded = (planet) => {
@@ -33,7 +33,7 @@ export default class randomPlanet extends Component {
         })
     };
 
-    updatePlanet() {
+    updatePlanet = () => {
         const id = Math.floor(Math.random() * 19 + 2);
         this.swapiService
             .getPlanet(id)
